5049211331e7db0175582074f8482289afcaf036
John Merz <jmerz826@gmail.com>
2022-01-27 21:39:10 -0500

    testing getById endpoint successfully

--- api/server.test.js
+++ api/server.test.js
@@ -17,10 +17,20 @@ test('sanity check', () => {
     expect(1).toBe(1)
 })
 
-describe('GET /api/bands', () => {
+describe('GET band(s) endpoints', () => {
     it('returns 200 status and all bands', async () => {
         const res = await request(server).get('/api/bands')
         expect(res.status).toBe(200)
         expect(res.body).toHaveLength(7)
+    })
+    it('returns 200 status and selected band via params', async () => {
+        const res = await request(server).get('/api/bands/1')
+        expect(res.status).toBe(200)
+        expect(res.body.band_name).toBe('Led Zeppelin')
+    })
+    it('returns correct error if band with "id" does not exist', async () => {
+        const res = await request(server).get('/api/bands/999')
+        expect(res.status).toBe(404)
+        expect(res.body.message).toBe("couldn't find band with id 999")
     })
 })
